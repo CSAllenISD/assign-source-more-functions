442dcd0ecedea3c265314dbee765b5de7f9cc359
John Williams <john.williams@allenisd.org>
2018-02-08 22:44:16 -0600

    Initial files for project

---
+++ README.md
@@ -0,0 +1,5 @@
+# Allen ISD Computer Science Assignment
+# More Functions
+# John Williams
+# Computer Science III, Period 5
+# 2018.02.09
---
+++ more-functions.swift
@@ -0,0 +1,153 @@
+// Allen ISD Computer Science Assignment
+// More Functions
+// John Williams
+// Computer Science III, Period 5
+// 2018.02.09
+
+/********************************************************************************
+As always, READ ALL INSTRUCTIONS!!!
+
+Throughout this lab (and all future labs) PAY CLOSE ATTENTION to the names
+you select for functions, parameters, variables, and constants.
+
+In this lab you MAY NOT USE ANY BUILT-IN FUNCTION other than the basic operators:
++, -, *, /, %, =, compound assignment operators, and relational operators.
+
+********************************************************************************/
+
+/********************************************************************************
+( 1) Define a function which accepts an integer argument and returns an integer 
+     twice as large.
+********************************************************************************/
+
+
+
+
+
+/********************************************************************************
+( 2) Define a function which accepts an integer argument and returns an integer 
+     five times as large.
+********************************************************************************/
+  
+
+
+
+
+
+/********************************************************************************
+( 3) Define a function which accepts an integer argument and returns an integer 
+     ten times as large. The function MAY NOT USE any constant literals.
+********************************************************************************/
+
+
+
+
+
+/********************************************************************************
+( 4) Define a function which accepts a double argument and returns an integer 
+     such that the integer returned is the greatest integer that is less than or 
+     equal to the argument (aka floor).
+********************************************************************************/
+
+
+
+
+
+/********************************************************************************
+( 5) Define a function which accepts a double argument and returns an integer
+     such that the integer returned is the least integer that is greater than or
+     equal to the argument (aka ceiling).
+********************************************************************************/
+
+
+
+
+  
+/********************************************************************************
+( 6) Define a function which accepts a string argument and returns a string
+     such that the string returned repeats the original string twice.
+********************************************************************************/  
+
+
+
+
+
+/********************************************************************************
+( 7) Define a function which accepts two arguments.  The first is a string and
+     the second is an integer indicating the  number of times to repeat that string.  
+     The returned value is a string which is repeated the specified number of times.
+********************************************************************************/  
+
+
+
+
+
+/********************************************************************************
+( 8) Define a function which accepts an integer argument and returns a string
+     such that the string returned contains the integer value, e.g. given
+     123 the function returns "123".
+********************************************************************************/
+
+
+
+
+
+/********************************************************************************
+( 9) Define a function which accepts a double argument and returns a string
+     such that the string returned contains the double value, e.g. given
+     123.45 the function returns "123.45".
+********************************************************************************/
+
+
+
+
+
+/********************************************************************************
+(10) Define a function which accepts an integer argument and returns an integer
+     such that the integer is the factorial of the argument.
+     The function should print each step of calculation.
+********************************************************************************/
+
+
+
+
+
+/********************************************************************************
+(11) Define a function which accepts an integer argument and returns an integer
+     such that the integer is the nth integer of the Fibonacci sequence, i.e.
+     specifying 0 yields 0, specifying 1 yields 1, specifying 2 yields 1, etc.
+     The function should print each step of the calculation.
+********************************************************************************/
+
+
+  
+  
+    
+/********************************************************************************
+(12) Define a function which accepts two double arguments representing the length
+     of the sides of a right triangle and returns a double such that the double
+     is the sum of each of the sides squared.
+********************************************************************************/
+
+
+
+
+
+/********************************************************************************
+(13) Define a function which accepts an integer argument and returns a boolean
+     value such that the boolean is true if and only if the argument is prime.
+********************************************************************************/
+
+
+
+
+
+/********************************************************************************
+(14) Define a function which accepts an integer argument and returns the count
+     of prime numbers less than or equal to the argument.
+     The function should print each such prime number found.
+********************************************************************************/  
+  
+  
+
+  
